9d17db90e1167801a617a702dc824797bc2b9ff7
mmustafaseker <mustiseker.06@gmail.com>
2021-04-21 13:15:30 +0300

    Sıralama

--- GuzelSozlerim/Controllers/HomeController.cs
+++ GuzelSozlerim/Controllers/HomeController.cs
@@ -1,23 +1,26 @@
 ﻿using GuzelSozlerim.Data;
+using GuzelSozlerim.Extensions;
 using GuzelSozlerim.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace GuzelSozlerim.Controllers
 {
-    [Authorize]
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger,ApplicationDbContext dbContext)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
             _dbContext = dbContext;
@@ -25,10 +28,43 @@ namespace GuzelSozlerim.Controllers
 
         public IActionResult Index()
         {
-            return View(_dbContext.GuzelSozler.ToList());
+            return View(_dbContext.GuzelSozler.Include(x => x.Begenenler).ToList());
         }
 
-  
+        [Authorize] //sadece üyeler kullanabilir
+        [HttpPost]
+        public IActionResult begenidurumuguncelle(int id, bool begenildiMi)
+        {
+            try
+            {
+                string userId = User.GetUserId();
+                var begeni = new KullaniciSoz() { GuzelSozId = id, KullaniciId = userId };
+                if (begenildiMi)
+                {
+                    if (!_dbContext.KullaniciSozler.Contains(begeni))
+                    {
+                        _dbContext.KullaniciSozler.Add(begeni);
+                    }
+                }
+                else
+                {
+                    if (_dbContext.KullaniciSozler.Contains(begeni))
+                    {
+                        _dbContext.KullaniciSozler.Remove(begeni);
+
+                    }
+                }
+                //_dbContext.Entry(begeni).State = begenildiMi ? EntityState.Added : EntityState.Deleted;
+                _dbContext.SaveChanges();
+                return new EmptyResult();
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
